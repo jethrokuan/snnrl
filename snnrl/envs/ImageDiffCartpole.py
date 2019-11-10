270436dd9ceda011186e41c0c3d28b2d4835c1c8
Jethro Kuan <jethrokuan95@gmail.com>
2019-11-10 21:08:38 +0800

    Invert colours

--- snnrl/envs/ImageDiffCartpole.py
+++ snnrl/envs/ImageDiffCartpole.py
@@ -12,6 +12,8 @@ from gym.utils import seeding
 from PIL import Image
 import numpy as np
 import torchvision.transforms as T
+from snnrl.utils import Invert
+
 
 class ImageDiffCartPoleEnv(gym.Env):
     """
@@ -51,45 +53,51 @@ class ImageDiffCartPoleEnv(gym.Env):
         Considered solved when the average reward is greater than or equal to 195.0 over 100 consecutive trials.
     """
 
-    metadata = {
-        'render.modes': ['human', 'rgb_array'],
-        'video.frames_per_second' : 50
-    }
+    metadata = {"render.modes": ["human", "rgb_array"], "video.frames_per_second": 50}
 
     def __init__(self):
         self.gravity = 9.8
         self.masscart = 1.0
         self.masspole = 0.1
-        self.total_mass = (self.masspole + self.masscart)
-        self.length = 0.5 # actually half the pole's length
-        self.polemass_length = (self.masspole * self.length)
+        self.total_mass = self.masspole + self.masscart
+        self.length = 0.5  # actually half the pole's length
+        self.polemass_length = self.masspole * self.length
         self.force_mag = 10.0
         self.tau = 0.02  # seconds between state updates
-        self.kinematics_integrator = 'euler'
+        self.kinematics_integrator = "euler"
 
         # Angle at which to fail the episode
         self.theta_threshold_radians = 12 * 2 * math.pi / 360
         self.x_threshold = 2.4
 
         # Angle limit set to 2 * theta_threshold_radians so failing observation is still within bounds
-        high = np.array([
-            self.x_threshold * 2,
-            np.finfo(np.float32).max,
-            self.theta_threshold_radians * 2,
-            np.finfo(np.float32).max])
+        high = np.array(
+            [
+                self.x_threshold * 2,
+                np.finfo(np.float32).max,
+                self.theta_threshold_radians * 2,
+                np.finfo(np.float32).max,
+            ]
+        )
 
         self.action_space = spaces.Discrete(2)
-        self.observation_space = spaces.Box(-high, high, dtype=np.float32) # NOTE: Don't use this!
+        self.observation_space = spaces.Box(
+            -high, high, dtype=np.float32
+        )  # NOTE: Don't use this!
 
         self.seed()
         self.viewer = None
         self.state = None
 
         self.steps_beyond_done = None
-        self.resize = T.Compose([T.ToPILImage(),
-                                 T.Resize(40, interpolation=Image.CUBIC),
-                                 T.ToTensor()])
-
+        self.resize = T.Compose(
+            [
+                T.ToPILImage(),
+                T.Resize(40, interpolation=Image.CUBIC),
+                Invert(),
+                T.ToTensor(),
+            ]
+        )
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -97,30 +105,40 @@ class ImageDiffCartPoleEnv(gym.Env):
 
     def step(self, action):
         self.last_screen = self.current_screen
-        assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
+        assert self.action_space.contains(action), "%r (%s) invalid" % (
+            action,
+            type(action),
+        )
         state = self.state
         x, x_dot, theta, theta_dot = state
-        force = self.force_mag if action==1 else -self.force_mag
+        force = self.force_mag if action == 1 else -self.force_mag
         costheta = math.cos(theta)
         sintheta = math.sin(theta)
-        temp = (force + self.polemass_length * theta_dot * theta_dot * sintheta) / self.total_mass
-        thetaacc = (self.gravity * sintheta - costheta* temp) / (self.length * (4.0/3.0 - self.masspole * costheta * costheta / self.total_mass))
-        xacc  = temp - self.polemass_length * thetaacc * costheta / self.total_mass
-        if self.kinematics_integrator == 'euler':
-            x  = x + self.tau * x_dot
+        temp = (
+            force + self.polemass_length * theta_dot * theta_dot * sintheta
+        ) / self.total_mass
+        thetaacc = (self.gravity * sintheta - costheta * temp) / (
+            self.length
+            * (4.0 / 3.0 - self.masspole * costheta * costheta / self.total_mass)
+        )
+        xacc = temp - self.polemass_length * thetaacc * costheta / self.total_mass
+        if self.kinematics_integrator == "euler":
+            x = x + self.tau * x_dot
             x_dot = x_dot + self.tau * xacc
             theta = theta + self.tau * theta_dot
             theta_dot = theta_dot + self.tau * thetaacc
-        else: # semi-implicit euler
+        else:  # semi-implicit euler
             x_dot = x_dot + self.tau * xacc
-            x  = x + self.tau * x_dot
+            x = x + self.tau * x_dot
             theta_dot = theta_dot + self.tau * thetaacc
             theta = theta + self.tau * theta_dot
-        self.state = (x,x_dot,theta,theta_dot)
-        done =  x < -self.x_threshold \
-                or x > self.x_threshold \
-                or theta < -self.theta_threshold_radians \
-                or theta > self.theta_threshold_radians
+        self.state = (x, x_dot, theta, theta_dot)
+        done = (
+            x < -self.x_threshold
+            or x > self.x_threshold
+            or theta < -self.theta_threshold_radians
+            or theta > self.theta_threshold_radians
+        )
         done = bool(done)
 
         if not done:
@@ -131,7 +149,9 @@ class ImageDiffCartPoleEnv(gym.Env):
             reward = 1.0
         else:
             if self.steps_beyond_done == 0:
-                logger.warn("You are calling 'step()' even though this environment has already returned done = True. You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior.")
+                logger.warn(
+                    "You are calling 'step()' even though this environment has already returned done = True. You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior."
+                )
             self.steps_beyond_done += 1
             reward = 0.0
 
@@ -145,13 +165,13 @@ class ImageDiffCartPoleEnv(gym.Env):
         self.current_screen = self.get_screen()
         return self.current_screen - self.last_screen
 
-    def render(self, mode='human'):
+    def render(self, mode="human"):
         screen_width = 600
         screen_height = 400
 
-        world_width = self.x_threshold*2
-        scale = screen_width/world_width
-        carty = 100 # TOP OF CART
+        world_width = self.x_threshold * 2
+        scale = screen_width / world_width
+        carty = 100  # TOP OF CART
         polewidth = 10.0
         polelen = scale * (2 * self.length)
         cartwidth = 50.0
@@ -159,44 +179,56 @@ class ImageDiffCartPoleEnv(gym.Env):
 
         if self.viewer is None:
             from gym.envs.classic_control import rendering
+
             self.viewer = rendering.Viewer(screen_width, screen_height)
-            l,r,t,b = -cartwidth/2, cartwidth/2, cartheight/2, -cartheight/2
-            axleoffset =cartheight/4.0
-            cart = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
+            l, r, t, b = -cartwidth / 2, cartwidth / 2, cartheight / 2, -cartheight / 2
+            axleoffset = cartheight / 4.0
+            cart = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
             self.carttrans = rendering.Transform()
             cart.add_attr(self.carttrans)
             self.viewer.add_geom(cart)
-            l,r,t,b = -polewidth/2,polewidth/2,polelen-polewidth/2,-polewidth/2
-            pole = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
-            pole.set_color(.8,.6,.4)
+            l, r, t, b = (
+                -polewidth / 2,
+                polewidth / 2,
+                polelen - polewidth / 2,
+                -polewidth / 2,
+            )
+            pole = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+            pole.set_color(0.8, 0.6, 0.4)
             self.poletrans = rendering.Transform(translation=(0, axleoffset))
             pole.add_attr(self.poletrans)
             pole.add_attr(self.carttrans)
             self.viewer.add_geom(pole)
-            self.axle = rendering.make_circle(polewidth/2)
+            self.axle = rendering.make_circle(polewidth / 2)
             self.axle.add_attr(self.poletrans)
             self.axle.add_attr(self.carttrans)
-            self.axle.set_color(.5,.5,.8)
+            self.axle.set_color(0.5, 0.5, 0.8)
             self.viewer.add_geom(self.axle)
-            self.track = rendering.Line((0,carty), (screen_width,carty))
-            self.track.set_color(0,0,0)
+            self.track = rendering.Line((0, carty), (screen_width, carty))
+            self.track.set_color(0, 0, 0)
             self.viewer.add_geom(self.track)
 
             self._pole_geom = pole
 
-        if self.state is None: return None
+        if self.state is None:
+            return None
 
         # Edit the pole polygon vertex
         pole = self._pole_geom
-        l,r,t,b = -polewidth/2,polewidth/2,polelen-polewidth/2,-polewidth/2
-        pole.v = [(l,b), (l,t), (r,t), (r,b)]
+        l, r, t, b = (
+            -polewidth / 2,
+            polewidth / 2,
+            polelen - polewidth / 2,
+            -polewidth / 2,
+        )
+        pole.v = [(l, b), (l, t), (r, t), (r, b)]
 
         x = self.state
-        cartx = x[0]*scale+screen_width/2.0 # MIDDLE OF CART
+        cartx = x[0] * scale + screen_width / 2.0  # MIDDLE OF CART
         self.carttrans.set_translation(cartx, carty)
         self.poletrans.set_rotation(-x[2])
 
-        return self.viewer.render(return_rgb_array = mode=='rgb_array')
+        return self.viewer.render(return_rgb_array=mode == "rgb_array")
 
     def close(self):
         if self.viewer:
@@ -211,10 +243,10 @@ class ImageDiffCartPoleEnv(gym.Env):
     def get_screen(self):
         # Returned screen requested by gym is 400x600x3, but is sometimes larger
         # such as 800x1200x3. Transpose it into torch order (CHW).
-        screen = self.render(mode='rgb_array').transpose((2, 0, 1))
+        screen = self.render(mode="rgb_array").transpose((2, 0, 1))
         # Cart is in the lower half, so strip off the top and bottom of the screen
         _, screen_height, screen_width = screen.shape
-        screen = screen[:, int(screen_height*0.4):int(screen_height * 0.8)]
+        screen = screen[:, int(screen_height * 0.4) : int(screen_height * 0.8)]
         view_width = int(screen_width * 0.6)
         cart_location = self.get_cart_location(screen_width)
         if cart_location < view_width // 2:
@@ -222,8 +254,9 @@ class ImageDiffCartPoleEnv(gym.Env):
         elif cart_location > (screen_width - view_width // 2):
             slice_range = slice(-view_width, None)
         else:
-            slice_range = slice(cart_location - view_width // 2,
-                                cart_location + view_width // 2)
+            slice_range = slice(
+                cart_location - view_width // 2, cart_location + view_width // 2
+            )
         # Strip off the edges, so that we have a square image centered on a cart
         screen = screen[:, :, slice_range]
         # Convert to float, rescale, convert to torch tensor
--- snnrl/utils.py
+++ snnrl/utils.py
@@ -1,21 +1,68 @@
 """Utility functions for model."""
 
+import torch
+from PIL import Image, ImageOps
+import numpy as np
+
+import torchvision
+import torchvision.transforms.functional as F
+
+
 import os
 import os.path as osp
 import shutil
 import torch
 
-def save_checkpoint(state,
-                    model,
-                    output_path,
-                    checkpoint,
-                    is_best=False):
+
+def save_checkpoint(state, model, output_path, checkpoint, is_best=False):
     """Saves the model in specified output path."""
     filepath = osp.join(output_path, "model_{}.pt".format(checkpoint))
     if not osp.exists(output_path):
         os.mkdir(output_path)
 
-    model_dict = { "model": model }
-    torch.save({ **state, **model_dict }, filepath)
+    model_dict = {"model": model}
+    torch.save({**state, **model_dict}, filepath)
     if is_best:
         shutil.copyfile(filepath, osp.join(output_path, "best.pt"))
+
+
+class Invert(object):
+    """Inverts the color channels of an PIL Image
+    while leaving intact the alpha channel.
+    """
+
+    def invert(self, img):
+        r"""Invert the input PIL Image.
+        Args:
+            img (PIL Image): Image to be inverted.
+        Returns:
+            PIL Image: Inverted image.
+        """
+        if not F._is_pil_image(img):
+            raise TypeError("img should be PIL Image. Got {}".format(type(img)))
+
+        if img.mode == "RGBA":
+            r, g, b, a = img.split()
+            rgb = Image.merge("RGB", (r, g, b))
+            inv = ImageOps.invert(rgb)
+            r, g, b = inv.split()
+            inv = Image.merge("RGBA", (r, g, b, a))
+        elif img.mode == "LA":
+            l, a = img.split()
+            l = ImageOps.invert(l)
+            inv = Image.merge("LA", (l, a))
+        else:
+            inv = ImageOps.invert(img)
+        return inv
+
+    def __call__(self, img):
+        """
+        Args:
+            img (PIL Image): Image to be inverted.
+        Returns:
+            PIL Image: Inverted image.
+        """
+        return self.invert(img)
+
+    def __repr__(self):
+        return self.__class__.__name__ + "()"
